1adbe05072f1cc294c2f414e044e9bd986c6931d
lazybabyz <lazybabyz@users.noreply.github.com>
2025-12-15 22:00:37 +0000

    deploy: 38eb967b31f4b5c84e418ab90b0c85be2a356f9b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9ea4e980.js',
+                path_to_root + 'searchindex-eb093163.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
