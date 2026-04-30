e9d81524d01b5b4e5f54c2689c1f0cdcf3b0f20f
lazybabyz <lazybabyz@users.noreply.github.com>
2026-04-30 22:01:16 +0000

    deploy: 5d0ae467843cb1608ca2fefcaf79e73f321ee926

--- toc-436c7b66.js
+++ toc-436c7b66.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="2026-04-30.html">Today</a></span></li><li class="chapter-item expanded "><li class="part-title">2026-04</li></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="daily/2026-04-30.html"><strong aria-hidden="true">1.</strong> 04-30-日刊</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="daily/2026-04-28.html"><strong aria-hidden="true">2.</strong> 04-28-日刊</a></span></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="2026-05-01.html">Today</a></span></li><li class="chapter-item expanded "><li class="part-title">2026-05</li></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="daily/2026-05-01.html"><strong aria-hidden="true">1.</strong> 05-01-日刊</a></span></li><li class="chapter-item expanded "><li class="part-title">2026-04</li></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="daily/2026-04-30.html"><strong aria-hidden="true">2.</strong> 04-30-日刊</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="daily/2026-04-28.html"><strong aria-hidden="true">3.</strong> 04-28-日刊</a></span></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split('#')[0].split('?')[0];
         if (current_page.endsWith('/')) {
